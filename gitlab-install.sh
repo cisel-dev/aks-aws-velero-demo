950fa370be78967ac135efbc09b7030d4ca53bbe
CISEL <76199462+cisel-dev@users.noreply.github.com>
2020-12-18 16:36:27 +0100

    Update gitlab-install.sh

--- gitlab-install.sh
+++ gitlab-install.sh
@@ -1,13 +1,9 @@
-################################################################################################################
-# GITLAB
-################################################################################################################
-
+# Generate a self-signed cert
 openssl req -x509 -newkey rsa:4096 -sha256 -nodes -keyout tlsgitlab.key -out tlsgitlab.crt -subj "/CN=gitlab.example.com" -days 365
 kubectl -n gitlab create secret tls gitlab-example-com --cert=tlsgitlab.crt --key=tlsgitlab.key
 
-kubectl create ns gitlab
-
 # GitLab Installation via Helm
+kubectl create ns gitlab
 helm --namespace gitlab install gitlab gitlab/gitlab \
   --set certmanager.install=false \
   --set global.ingress.configureCertmanager=false \
@@ -24,6 +20,7 @@ helm --namespace gitlab install gitlab gitlab/gitlab \
 
 # Get GitLab web root password
 kubectl -n gitlab get secret gitlab-gitlab-initial-root-password -ojsonpath='{.data.password}' | base64 --decode ; echo
+
 # Annotate pods to allow Velero to backup their PV with restic
 kubectl annotate pod/gitlab-postgresql-0 backup.velero.io/backup-volumes=data -n gitlab
 kubectl annotate pod/gitlab-gitaly-0 backup.velero.io/backup-volumes=repo-data -n gitlab
